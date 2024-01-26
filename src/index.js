b9ef34a0a9713235bb64cbba160e7530f18fa49a
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-25 19:54:05 -0800

    main steps of the game

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@
 /********Content*************************************************** */
 const questions = [
     {
-        question: "1",
+        question: "There are 7 days in week.",
         answer: true
     },
     {
-        question: "2",
-        answer: true
-    },
-    {
-        question: "3",
+        question: "It's 2022 year now.",
         answer: false
     },
     {
-        question: "4",
+        question: "There are 60 seconds in a minute.",
         answer: true
     },
     {
-        question: "5",
+        question: "We are living in Mars.",
+        answer: false
+    },
+    {
+        question: "The Earth is round.",
         answer: true
     }
 ];
@@ -26,7 +26,7 @@ const questions = [
 const pictureParts = [
     {
         id: 4,
-        points: [[1, 1], [1, 2], [1, 7], [1, 8], [1, 9], [2, 1], [2, 3], [2, 6], [2, 9], [3, 1], [3, 4], [3, 5], [3, 9], [4, 1], [4, 9], [5, 1], [5, 9], [6, 1], [6, 3], [6, 6], [6, 9], [6, 13], [6, 14], [7, 1], [7, 3], [7, 6], [7, 9], [7, 12], [7, 15], [8, 1], [8, 9], [8, 10], [8, 12], [8, 14], [8, 15], [9, 2], [9, 8], [9, 11], [9, 14], [10, 3], [10, 12], [10, 13], [11, 3], [11, 12], [12, 3],[12,5],[12,7],[12,10], [12, 12], [13, 3],[13,5],[13,7], [13, 8],[13,9],[13,10], [13, 12], [14, 3], [14, 4], [14, 5], [14, 6], [14, 7], [14, 9], [14, 10], [14, 11], [14, 12]],
+        points: [[1, 1], [1, 2], [1, 7], [1, 8], [1, 9], [2, 1], [2, 3], [2, 6], [2, 9], [3, 1], [3, 4], [3, 5], [3, 9], [4, 1], [4, 9], [5, 1], [5, 9], [6, 1], [6, 3], [6, 6], [6, 9], [6, 13], [6, 14], [7, 1], [7, 3], [7, 6], [7, 9], [7, 12], [7, 15], [8, 1], [8, 9], [8, 10], [8, 12], [8, 14], [8, 15], [9, 2], [9, 8], [9, 11], [9, 14], [10, 3], [10, 12], [10, 13], [11, 3], [11, 12], [12, 3], [12, 5], [12, 7], [12, 10], [12, 12], [13, 3], [13, 5], [13, 7], [13, 8], [13, 9], [13, 10], [13, 12], [14, 3], [14, 4], [14, 5], [14, 6], [14, 7], [14, 9], [14, 10], [14, 11], [14, 12]],
         color: "black"
     },
     {
@@ -73,32 +73,47 @@ app.style.textAlign = "center"
 app.appendChild(document.createElement("h1")).textContent = "Let's find the hidden picture";
 
 app.appendChild(createTable());
-let cntrCell = document.querySelector("td");
+
 /*************************************************************************** */
-
-let startQuestion = window.confirm("Let's start our game!");
-
-if (startQuestion ){
-    console.log("start")
-for (let questionId in questions) {
-    if (askQuestion(questions[questionId])) {
-        console.log(questionId)
-        let picpart = pictureParts.filter((part) => part.id == Number(questionId) );
-        console.log(picpart)
-        colorPicture(picpart);
-        cntrCell.style.background = "green";
+let startQuestion;
+setTimeout(() => {
+    startQuestion = window.confirm("Let's start our game!");
+    if (startQuestion) {
+        let madeMistake = false;
+        console.log("start")
+        for (let questionId in questions) {
+            let check;
+            setTimeout(() => { 
+                check = askQuestion(questions[questionId]); 
+                if (check) {
+                    colorPicture(pictureParts.filter((part) => part.id == Number(questionId))[0]);
+                    sleep(200);
+                }
+                else madeMistake = true;
+            }, 300);
+            
+            //sleep(500);
+        }
+        
     }
-    else {
-        console.log("wrong")
-        cntrCell.style.background = "red";
-    }
- 
-}
-}
+},300);
 
-
-
-
+// if (startQuestion) {
+//     let madeMistake = false;
+//     console.log("start")
+//     for (let questionId in questions) {
+//         let check;
+//         setTimeout(() => { check = askQuestion(questions[questionId]) }, 300);
+//         if (check) {
+//             colorPicture(pictureParts.filter((part) => part.id == Number(questionId))[0]);
+//             window.top.focus();
+//             sleep(2000);
+//         }
+//         else madeMistake = true;
+//         //sleep(500);
+//     }
+//     if (madeMistake) window.alert("Try again!");
+// }
 
 /********************************************************* */
 /*****Functions******************************************* */
@@ -121,13 +136,24 @@ function createTable() {
 }
 
 function askQuestion(question) {
-    let answer= window.confirm(question.question);
+    let answer = window.confirm(question.question + "\nPress 'OK' if 'Yes' and 'Cancel' if 'No'!");
     return answer == question.answer;
 }
 
-function colorPicture (picPart){
+function colorPicture(picPart) {
     let table = document.querySelector("table");
-     for (const point of picPart[0].points) {
-         table.childNodes[point[0]].childNodes[point[1]].style.background = picPart[0].color;
+    for (const point of picPart.points) {
+        table.childNodes[point[0]].childNodes[point[1]].style.background = picPart.color;
     }
 }
+
+    
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
